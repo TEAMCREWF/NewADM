cef6cfdfbe2c99b4e887b6294bfe0452592eb0f3
TEAMCREWF <60835488+TEAMCREWF@users.noreply.github.com>
2020-02-09 22:16:10 -0600

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IVAR="/etc/http-instas"
 SCPT_DIR="/etc/SCRIPT"
-SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2JhcmJhOTkvbmV3YWQvbWFzdGVyL2dlcmFkb3I="
+SCPresq="aHR0cHM6Ly9naXRodWIuY29tL1RFQU1DUkVXRi9uZXdhZG0vdHJlZS9tYXN0ZXIvZ2VyYWRvcg=="
 SUB_DOM='base64 -d'
 rm $(pwd)/$0
 ofus () {
